838797537c4adaf5f4f9709c027d7f0f2ae791cf
Namgad <38654700+Namgad@users.noreply.github.com>
2020-02-04 23:57:38 +0530

    Create TimeTable.java
    Time Table Generator using random generator
    Test Cases : max off lectures 0, 1, 2

---
+++ TimeTable.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class TimeTable {
+    public static void GenerateTable(int arr[][],int nos,String day[], String fac_name[],int freeLectures){
+        int k=0;
+        Random r = new Random();
+
+        HashMap<Integer,String> hashmap = new HashMap<Integer, String>();
+        hashmap.put(0,"0");
+        int id=1;
+        for(int i=0 ; i<nos ; i++){
+            hashmap.put(id,fac_name[i]);
+            id++;
+        }
+        System.out.println(hashmap);
+        //Timetable Generates from here
+        while(k<6) {
+            int count=0;
+            ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+            for(int i=0 ; i<nos ; i++){
+                int random=r.nextInt(nos+1);
+                if(random==0 && freeLectures==0)
+                {
+                 i--;
+                }
+                else {
+
+                    if (arrayList.contains(random)) {
+                        if (random == 0 && count < freeLectures) {
+                            arrayList.add(random);
+                            count++;
+                        } else {
+                            i--;
+                        }
+                    } else {
+                        if (random == 0 && count < freeLectures && count != freeLectures) {
+                            arrayList.add(random);
+                            count++;
+                        } else {
+                            arrayList.add(random);
+                        }
+                    }
+                }
+
+            }
+
+            //System.out.println("ArrayList: "+arrayList);
+            for(int j=0 ; j<nos ; j++){
+                arr[k][j] = arrayList.get(j);
+            }
+            k++;
+        }
+
+        //Printing TimeTable
+        for(int i=0 ; i<6 ; i++){
+            System.out.print(day[i]+" ");
+            for(int j=0 ; j<nos ; j++){
+//                System.out.print(arr[i][j]+" ");
+                System.out.format(hashmap.get(arr[i][j])+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Where 0 = Free Lecture");
+    }
+    public static void main(String args[])
+    {
+
+        String[] day=new String[6];
+        day[0] = "Monday    ";
+        day[1] = "Tuesday   ";
+        day[2] = "Wednesday ";
+        day[3] = "Thursday  ";
+        day[4] = "Friday    ";
+        day[5] = "Saturday  ";
+
+        int nos,i,j;
+
+        Scanner sc=new Scanner(System.in);
+
+        //Entering Number Of Subjects
+        System.out.println("Enter Number Of Subjects : ");
+        nos=sc.nextInt();
+        //nos = nos+1;
+
+        String fac_name[] = new String[nos];
+        int A[][] = new int[6][nos];
+
+        //Giving Id to teacher
+        System.out.println("Enter  name  of  faculty  with  assigned  subject in the format name_of_faculty(Subject name) :");
+
+        for(i=0 ; i<nos ; i-=-1){
+//            System.out.println("i is "+i);
+            fac_name[i]=sc.next();
+        }
+
+        System.out.println("Enter Maximum Number of Free Lectures to be Given :");
+        int freeLectures=sc.nextInt();
+        GenerateTable(A,nos,day,fac_name,freeLectures);
+    }
+}
